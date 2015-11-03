3b9ba4480b14ca6879e13b48e933d758fddb810f
Eric Ashmore <edashmore@gmail.com>
2015-11-02 18:49:36 -0800

    implement attr, addClass and removeClass

--- jquery_lite.js
+++ jquery_lite.js
@@ -16,52 +16,64 @@
   };
 
   var DOMNodeCollection = function(array) {
-    this.htmlElements = array;
+    this.nodes = array;
   };
 
   DOMNodeCollection.prototype.html = function(str) {
     if (str) {
-      this.htmlElements.forEach(function(el) {
+      this.nodes.forEach(function(el) {
         el.innerHTML = str;
       });
     } else {
-      return this.htmlElements[0].innerHTML;
+      return this.nodes[0].innerHTML;
     }
   };
 
   DOMNodeCollection.prototype.empty = function() {
     this.html("");
 
-    this.htmlElements = [];
+    this.nodes = [];
   };
 
   DOMNodeCollection.prototype.append = function(arg) {
     var html;
 
     if (typeof arg === 'string') {
-      this.htmlElements.forEach(function(el) {
+      this.nodes.forEach(function(el) {
         html = el.innerHTML;
         el.innerHTML = html + arg;
       });
     } else if (typeof arg === 'object') {
-      this.htmlElements.forEach(function(el) {
+      this.nodes.forEach(function(el) {
         html = el.innerHTML;
-        arg.htmlElements.forEach(function(collectionEl) {
+        arg.nodes.forEach(function(collectionEl) {
           el.innerHTML = html + collectionEl.outerHTML;
         });
       });
     }
   };
 
-  DOMNodeCollection.prototype.attr = function() {
+  DOMNodeCollection.prototype.attr = function(str, value) {
+    var attribute = this.nodes[0].getAttribute(str);
 
+    if (typeof value === 'string') {
+      this.nodes.forEach(function(node) {
+        node.setAttribute(str, value);
+      });
+    } else {
+      return attribute;
+    }
   };
 
-  DOMNodeCollection.prototype.addClass = function() {
-    
+  DOMNodeCollection.prototype.addClass = function(newClass) {
+    this.nodes.forEach(function(node) {
+      node.classList.add(newClass);
+    });
   };
 
-  DOMNodeCollection.prototype.removeClass = function() {
-
+  DOMNodeCollection.prototype.removeClass = function(oldClass) {
+    this.node.forEach(function(node) {
+      node.classList.remove(oldClass);
+    });
   };
 })();
